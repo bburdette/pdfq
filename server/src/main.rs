60f24d3fa5f648fe7a544f1302aa8eafc02cd2ec
Ben Burdette <bburdette@gmail.com>
2019-11-08 23:02:39 -0700

    upgrade to actix 1.0.8

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
-extern crate actix;
+extern crate actix_files;
+extern crate actix_rt;
 extern crate actix_web;
 extern crate env_logger;
 extern crate failure;
 extern crate futures;
+extern crate json;
 extern crate rand;
 extern crate serde_json;
 extern crate time;
@@ -11,25 +13,26 @@ extern crate uuid;
 #[macro_use]
 extern crate log;
 
-use actix_web::fs::NamedFile;
+use actix_files::NamedFile;
 use actix_web::http::{Method, StatusCode};
 use actix_web::middleware::Logger;
-use actix_web::Binary;
+// use actix_web::Binary;
 use actix_web::{
-  http, server, App, AsyncResponder, HttpMessage, HttpRequest, HttpResponse, Responder, Result,
+  http, middleware, web, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 // use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[macro_use]
 extern crate serde_derive;
 
-use failure::Error;
+// use failure::Error;
 use futures::future::Future;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
 use std::time::SystemTime;
 mod process_json;
 mod util;
+use json::JsonValue;
 use process_json::{process_public_json, PublicMessage, ServerResponse};
 
 #[derive(Deserialize, Debug, Clone)]
@@ -39,16 +42,23 @@ pub struct Config {
   pdfdir: String,
 }
 
-fn files(req: &HttpRequest<Config>) -> Result<NamedFile> {
+fn files(req: &HttpRequest) -> Result<NamedFile> {
   println!("files!");
-  let path: PathBuf = req.match_info().query("tail")?;
+  let path: PathBuf = req.match_info().query("tail").parse()?;
   info!("files: {:?}", path);
   let stpath = Path::new("static/").join(path);
   Ok(NamedFile::open(stpath)?)
 }
 
-fn pdffiles(req: &HttpRequest<Config>) -> Result<NamedFile> {
-  let path: PathBuf = req.match_info().query("tail")?;
+// fn index(state: web::Data<Mutex<usize>>, req: HttpRequest) -> HttpResponse {
+//     println!("{:?}", req);
+//     *(state.lock().unwrap()) += 1;
+
+//     HttpResponse::Ok().body(format!("Num of requests: {}", state.lock().unwrap()))
+// }
+
+fn pdffiles(state: web::Data<Config>, req: &HttpRequest) -> Result<NamedFile> {
+  let path: PathBuf = req.match_info().query("tail").parse()?;
   println!("pdffiles! {:?}", path);
   println!("pdffiles! {:?}", req.match_info());
   let uripath = Path::new(req.uri().path());
@@ -59,7 +69,7 @@ fn pdffiles(req: &HttpRequest<Config>) -> Result<NamedFile> {
     .map_err(|e| actix_web::error::ErrorImATeapot(e))
     .and_then(|path| {
       println!("alsopath: {:?}", path);
-      let stpath = Path::new(&req.state().pdfdir.to_string()).join(path);
+      let stpath = Path::new(&state.pdfdir.to_string()).join(path);
       println!("stpath: {:?}", stpath);
       let nf = NamedFile::open(stpath.clone());
       match nf {
@@ -75,18 +85,19 @@ fn pdffiles(req: &HttpRequest<Config>) -> Result<NamedFile> {
   // info!("stpath: {:?}", stpath);
 }
 
-fn favicon(_req: &HttpRequest<Config>) -> Result<NamedFile> {
+fn favicon(_req: &HttpRequest) -> Result<NamedFile> {
   let stpath = Path::new("static/favicon.ico");
   Ok(NamedFile::open(stpath)?)
 }
 
-fn sitemap(_req: &HttpRequest<Config>) -> Result<NamedFile> {
+fn sitemap(_req: &HttpRequest) -> Result<NamedFile> {
   let stpath = Path::new("static/sitemap.txt");
   Ok(NamedFile::open(stpath)?)
 }
 
 // simple index handler
-fn mainpage(req: &HttpRequest<Config>) -> Result<HttpResponse> {
+fn mainpage(state: web::Data<Config>, req: HttpRequest) -> HttpResponse {
+  println!("mainpage");
   info!(
     "remote ip: {:?}, request:{:?}",
     req.connection_info().remote(),
@@ -95,20 +106,50 @@ fn mainpage(req: &HttpRequest<Config>) -> Result<HttpResponse> {
 
   match util::load_string("static/index.html") {
     Ok(s) => {
+      println!("okaey");
       // response
-      Ok(
-        HttpResponse::build(StatusCode::OK)
-          .content_type("text/html; charset=utf-8")
-          .body(s),
-      )
+      HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(s)
     }
-    Err(e) => Err(e.into()),
+    Err(e) => {
+      println!("err");
+      HttpResponse::from_error(e.into())
+    }
   }
 }
 
-fn public(req: &HttpRequest<Config>) -> Box<dyn Future<Item = String, Error = Error>> {
+
+fn publick(
+  state: web::Data<Config>,
+  item: web::Json<PublicMessage>,
+  req: HttpRequest,
+) -> HttpResponse {
+  println!("model: {:?}", &item);
+
   let ci = req.connection_info().clone();
-  let pd = req.state().pdfdir.clone();
+  let pd = state.pdfdir.clone();
+
+
+  match process_json::process_public_json(pd.as_str(), &(ci.remote()), item.into_inner()) {
+    Ok(sr) =>
+      HttpResponse::Ok().json(sr),
+    Err(e) => {
+      error!("uh oh, 'public' err: {:?}", e);
+      let se = ServerResponse {
+        what: "server error".to_string(),
+        content: serde_json::Value::String(e.to_string()),
+      };
+      HttpResponse::Ok().json(se)
+    }
+  }
+
+}
+
+/*
+fn public(state: web::Data<Config>, req: HttpRequest) -> Box<dyn Future<Item = String, Error = actix_web::Error>> {
+  let ci = req.connection_info().clone();
+  let pd = state.pdfdir.clone();
   req
     .json()
     .from_err()
@@ -134,7 +175,37 @@ fn public(req: &HttpRequest<Config>) -> Box<dyn Future<Item = String, Error = Er
       )
     })
     .responder()
-}
+}*/
+
+// fn public(req: &HttpRequest) -> Box<dyn Future<Item = String, Error = Error>> {
+//   let ci = req.connection_info().clone();
+//   let pd = req.state().pdfdir.clone();
+//   req
+//     .json()
+//     .from_err()
+//     .and_then(move |msg: PublicMessage| {
+//       Ok(
+//         match process_json::process_public_json(pd.as_str(), &(ci.remote()), msg) {
+//           Ok(sr) => match serde_json::to_string(&sr) {
+//             Ok(s) => s,
+//             Err(e) => e.to_string(),
+//           },
+//           Err(e) => {
+//             error!("uh oh, 'public' err: {:?}", e);
+//             let se = ServerResponse {
+//               what: "server error".to_string(),
+//               content: serde_json::Value::String(e.to_string()),
+//             };
+//             match serde_json::to_string(&se) {
+//               Ok(s) => s,
+//               Err(e) => e.to_string(),
+//             }
+//           }
+//         },
+//       )
+//     })
+//     .responder()
+// }
 
 /*
 fn binfile(req: &HttpRequest) -> Box<Future<Item = Binary, Error = Error>> {
@@ -181,7 +252,7 @@ fn main() {
     Ok(_) => (),
   }
 }
-fn err_main() -> Result<(), Error> {
+fn err_main() -> Result<(), std::io::Error> {
   env_logger::init();
 
   info!("server init!");
@@ -190,7 +261,8 @@ fn err_main() -> Result<(), Error> {
 
   println!("config: {:?}", config);
 
-  let sys = actix::System::new("whatevs");
+  // let sys = actix::System::new("whatevs");
+  let sys = actix_rt::System::new("example");
 
   let nf = NamedFile::open("/home/bburdette/papers/7Sketches2.pdf");
   match nf {
@@ -198,25 +270,41 @@ fn err_main() -> Result<(), Error> {
     Err(e) => println!("err: {}", e),
   }
 
-  {
-    let c = config.clone();
-    let s = server::new(move || {
-      App::with_state(c.clone())
-        .resource("/public", |r| r.method(Method::POST).f(public))
-        //            .resource("/binfile", |r| r.method(Method::POST).f(binfile))
-        .resource("/favicon.ico", |r| r.method(Method::GET).f(favicon))
-        .resource("/sitemap.txt", |r| r.method(Method::GET).f(sitemap))
-        .resource(r"/", |r| r.method(Method::GET).f(mainpage))
-        .resource(r"/pdfs/{tail:.*}", |r| r.method(Method::GET).f(pdffiles))
-        .resource(r"/{tail:.*}", |r| r.method(Method::GET).f(files))
-    });
+  let c = web::Data::new(config.clone());
+  HttpServer::new(move || {
+    App::new()
+      .register_data(c.clone()) // <- create app with shared state
+      // enable logger
+      .wrap(middleware::Logger::default())
+      // register simple handler, handle all methods
+      // .route("/pdfs/{filename:.*}", web::get().to(pdffiles))
+      // .route("/public", |r| web::post().to(public))
+      .route("/", web::get().to(mainpage))
+      .route("/public", web::post().to(publick))
+      .service(actix_files::Files::new("/pdfs", c.pdfdir.as_str()))
+      .service(actix_files::Files::new("/", "static/"))
+    //   .service(web::resource("/pdfs").to(pdffiles))
+  })
+  .bind(format!("{}:{}", config.ip, config.port))?
+  .run()
 
-    s.bind(format!("{}:{}", config.ip, config.port))
-  }
-  .expect(format!("Can not bind to port {}", config.port).as_str())
-  .start();
+  // {
+  //   let c = config.clone();
+  //   let s = server::new(move || {
+  //     App::with_state(c.clone())
+  //       .resource("/public", |r| r.method(Method::POST).f(public))
+  //       //            .resource("/binfile", |r| r.method(Method::POST).f(binfile))
+  //       .resource("/favicon.ico", |r| r.method(Method::GET).f(favicon))
+  //       .resource("/sitemap.txt", |r| r.method(Method::GET).f(sitemap))
+  //       .resource(r"/", |r| r.method(Method::GET).f(mainpage))
+  //       .resource(r"/pdfs/{tail:.*}", |r| r.method(Method::GET).f(pdffiles))
+  //       .resource(r"/{tail:.*}", |r| r.method(Method::GET).f(files))
+  //   });
 
-  sys.run();
+  //   s.bind(format!("{}:{}", config.ip, config.port))
+  // }
+  // .expect(format!("Can not bind to port {}", config.port).as_str())
+  // .start();
 
-  Ok(())
+  // sys.run();
 }
