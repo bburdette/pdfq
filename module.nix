8b258bb20591d74a11e147ffc0ba9e5c62c7f9f2
Ben Burdette <bburdette@gmail.com>
2020-06-08 21:26:12 -0600

    module

---
+++ module.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.pdfq;
+
+  # Command line arguments for the pdfq daemon
+
+in
+
+{
+
+  ###### interface
+
+  options = {
+    services.pdfq = {
+      enable = mkEnableOption "pdfq";
+
+      };
+  };
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    systemd.services.pdfq = {
+      description = "pdfq";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig.User = "bburdette";
+
+      script = ''
+          cd /home/bburdette/code/pdfq/server
+          /home/bburdette/code/pdfq/server/target/debug/server
+          '';
+
+      # serviceConfig = {
+      #   User = "bburdette";
+      #   # Group = "bburdette";
+      #   ExecStart = ''
+      #     cd /home/bburdette/code/pdfq/server &&
+      #     /home/bburdette/code/pdfq/server/target/debug/server
+      #     '';
+      # };
+    };
+  };
+}
