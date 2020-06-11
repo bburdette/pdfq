f162fa3d66c67fbf51601ec7593c2b3deefd16d3
Ben Burdette <bburdette@gmail.com>
2020-06-11 12:10:09 -0600

    server and ui ending up in the same store pkg

--- nixpkg/default.nix
+++ nixpkg/default.nix
@@ -14,25 +14,31 @@ rustPlatform.buildRustPackage rec {
   pname = "pdfq";
   version = "1.0";
 
-  # ui = callPackage ./ui.nix { };
+  ui = callPackage ./ui.nix { };
 
   src = fetchFromGitHub {
     owner = "bburdette";
-    repo = pname;
-    rev = "b3f680260c65dc613371dce8b9045bb8122c5803";
-    sha256 = "1xjqvxp5gcnj9cgh28g5vvj0829i87y39zclnqgw4w36fxfhx9bp";
+    repo = "pdfq";
+    rev = "25e29e4f432eca12f84a8c71a295e3d185bc5a90";
+    sha256 = "1k0x8z6m2326prsmz3mrs9yjn6kq92jaqw74if77di6pwz26qcig";
   };
 
   # preBuild = ''
   #   cp -r ${ui}/libexec/gotify-ui/deps/gotify-ui/build ui/build && packr
   # '';
 
+  postInstall = ''
+    echo "postInttall"
+    ls -l $out
+    cp -r ${ui}/static $out
+  '';
+
   # cargo-culting this from the gotify package.
   subPackages = [ "." ];
 
 
   sourceRoot = "source/server";
-  cargoSha256 = "117r9z48wszi2y0wshxmd4nawd472zrg1qiz91if991bijv3pl9m";
+  cargoSha256 = "1jdbjx3xa7f4yhq4l7xsgy6jpdr2lkgqrzarqb5vj2s3jg13kyl4";
   # dontMakeSourcesWritable=1;
 
   buildInputs = [(stdenv.lib.optional stdenv.isDarwin Security) openssl sqlite];
