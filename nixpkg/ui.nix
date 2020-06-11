cf90bca5f72d67f2e03b5e867099d769b2ab2ea6
Ben Burdette <bburdette@gmail.com>
2020-06-10 18:35:33 -0600

    almost...

--- nixpkg/ui.nix
+++ nixpkg/ui.nix
@@ -9,25 +9,40 @@ yarn2nix-moretea.mkYarnPackage rec {
 
   name = "pdfq-ui";
 
-  packageJSON = ../elm/package.json;
-  yarnNix = ./yarn.nix;
+  # packageJSON = ../elm/package.json;
+  # yarnLock = ../elm/yarn.lock;
+  # yarnNix = ./yarn.nix;
 
   version = "1.0";
 
   src = fetchFromGitHub {
     owner = "bburdette";
     repo = "pdfq";
-    rev = "5918f1bc2b7d9de99968d7e973657c137ef70a91";
-    sha256 = "0fh8gdy5cf8n0dxbqyf1rzp05i934drmysdyl1z9zyx80bwiqkr3";
-  };
- 
-  configurePhase = elmPackages.fetchElmDeps {
-    elmPackages = import ./elm/elm-srcs.nix;
-    registryDat = ./elm/versions.dat;
-    elmVersion = "0.19.1";
+    rev = "70c967b04651f079c812561ad5eaa0a160926c1f";
+    sha256 = "074zyk6avm8k54y635wwgw7jvb277x1g17xw9plfb16k5c3qj6aw";
   };
 
-  nativeBuildInputs = [ utillinux elmPackages.elm ];
+  packageJSON = "${src}/elm/package.json";
+  yarnLock = "${src}/elm/yarn.lock";
+
+  # let this build the elm stuff, not parcel.
+  # the_elm = "./elm/default.nix";
+
+  preConfigure = ''
+    echo "preconfigure"
+    '';
+
+  # replacing a needed mkYarnPackage configurePhase?
+  # configurePhase = elmPackages.fetchElmDeps {
+  #   elmPackages = import ./elm/elm-srcs.nix;
+  #   registryDat = ./elm/versions.dat;
+  #   elmVersion = "0.19.1";
+  # };
+  #
+  # configurePhase = "";
+
+  nativeBuildInputs = [ utillinux # lscpu for parcel.. necessary?
+            ];
 
   # src = "${src_all}/ui";
 
@@ -35,21 +50,33 @@ yarn2nix-moretea.mkYarnPackage rec {
     # ./node-packages/.bin/parcel index.html --out-dir=$out/static
 
   # parcel = yarnNix.packages.
-  
 
-  postBuild = ''
-    runHook linkNodeModulesHook
+  preBuild = ''
+    echo "prebuild"
+    ls -l
+    '';
+
+  # postBuild = ''
+  buildPhase = ''
+    # runHook linkNodeModulesHook   # doesn't work
     pwd
+    # we need the node_modules because parcel is in there.
     ln -s $node_modules node_modules
+    ln -s $src src
+    echo "ls -al"
     ls -al
-    echo "node_modeules?$node_modules"
+    # ln -s .elm elm/.elm  # elm already built, don't need.
+    echo "node_modules: $node_modules"
     # find nixpkg
     echo "out: $out"
     # find $out
     # find ../../bin
     # ls ../../nix
     # find ../../etc
-    ./node_modules/.bin/parcel build index.html --out-dir=$out/static
+    cd src/elm
+    pwd
+    ls ../../
+    /build/source/node_modules/.bin/parcel build index.html --out-dir=$out/static
     '';
 
   # buildPhase = ''
